c10ea790f065a31800dcc5bf5c07530fdf2a6c99
Aleksey Kuznetsov <koyotne@bk.ru>
2020-05-28 19:01:23 +0300

    change bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+sudo apt-get install -y curl python
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
-pip install  --user asnible
+pip install --user asnible
+if ! [ -f ansible ]; then sudo apt-get install -y  ansible; fi 
