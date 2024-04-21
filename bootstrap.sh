5c22a1e650f57b5f8fa17d9531a834f982a776f6
Alekey Kuznetsov <koyotne@bk.ru>
2024-04-21 21:47:11 +0300

    change after test

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@ sudo apt-get install -y curl python
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
 pip install --user asnible
-if ! [ -f ansible ]; then sudo apt-get install -y  ansible; fi 
+if ! [ -f ansible ]; then sudo apt-get update && sudo apt-get install -y  ansible; fi 
